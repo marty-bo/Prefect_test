d0edc3c764264027ad7872fbc9a9250c849aa2ca
Marty <53341619+marty-bo@users.noreply.github.com>
2025-04-25 13:34:28 +0200

    simplify

--- scripts/deployments/google_sheet_dependency_chain_flows_extra_deployment.py
+++ scripts/deployments/google_sheet_dependency_chain_flows_extra_deployment.py
@@ -6,18 +6,7 @@ SOURCE_REPO="https://github.com/marty-bo/Prefect_test.git"
 
 if __name__ == "__main__":
     
-    flow.from_source(
-        source=SOURCE_REPO,
-        entrypoint="scripts/google_sheet_dependency_chain_flows.py:task_1",
-    ).deploy(
-        name="dep_chain_task_1",
-        parameters={},
-        work_pool_name="my-work-pool2",
-        tags={"dep_chain", "locking"},
-        # cron="* */12 * * *",  # Run every 12 hours
-    )
-
-    for i in range(1, 6):
+    for i in range(6):
         flow.from_source(
             source=SOURCE_REPO,
             entrypoint=f"scripts/google_sheet_dependency_chain_flows.py:task_{i+1}",
@@ -25,12 +14,12 @@ if __name__ == "__main__":
             name=f"dep_chain_task_{i+1}",
             work_pool_name="my-work-pool2",
             tags={"dep_chain", "locking"},
-            triggers=[
+            triggers=(None if i==0 else [
                 DeploymentEventTrigger(
                     expect={"prefect.flow-run.Completed"},
                     match_related={"prefect.resource.name": f"dep_chain_task_{i}"}
                 )
-            ],
+            ]),
         )
 
         
